47b78772643e48e71a08e1e13c8471bf6de15d26
corinthian <ianyh@mit.edu>
2010-09-02 17:32:01 -0400

    urls

---
+++ urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+                       (r'^$', 'grocheris.views.index'),
+                       (r'^stock/$', 'grocheris.views.view_in_stock'),
+                       (r'^shop/$', 'grocheris.views.view_shopping_list'),
+                       (r'^item/(?P<item_id>\d+)/$', 'grocheris.views.view_item'),
+                       (r'^item/(?P<item_id>\d+)/tag/$', 'grocheris.views.tag_item'),
+                       (r'^item/(?P<item_id>\d+)/vote/$', 'grocheris.views.vote'),
+                       )
+
