561bd339eb3fa68dc0d2fa119631288ab7f60454
corinthian <ianyh@mit.edu>
2010-09-04 17:19:32 -0400

    generalize urls in templates

--- urls.py
+++ urls.py
@@ -2,14 +2,11 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('grocheris.views',
                        (r'^$', 'index'),
-                       (r'^stock/$', 'view_in_stock'),
-                       (r'^shop/$', 'view_shopping_list'),
-                       (r'^add_item/$', 'views.add_item'),
-#                       (r'^item/(?P<item_id>\d+)/$', 'view_item'),
-                       (r'^item/(?P<item_id>\d+)/buy/$', 'buy_item'),
-                       (r'^item/(?P<item_id>\d+)/low/$', 'low_item'),
-                       (r'^item/(?P<item_id>\d+)/out/$', 'out_item'),
-#                       (r'^item/(?P<item_id>\d+)/tag/$', 'tag_item'),
-#                       (r'^item/(?P<item_id>\d+)/vote/$', 'vote'),
+                       url(r'^stock/$', 'view_in_stock', name='stock'),
+                       url(r'^shop/$', 'view_shopping_list', name='shop'),
+                       url(r'^add_item/$', 'add_item', name='add_item'),
+                       url(r'^item/(?P<item_id>\d+)/buy/$', 'buy_item', name='buy'),
+                       url(r'^item/(?P<item_id>\d+)/low/$', 'low_item', name='low'),
+                       url(r'^item/(?P<item_id>\d+)/out/$', 'out_item', name='out'),
                        )
 
