f2a8461b04ea980e958e86d394b4ba8168f975d7
corinthian <ianyh@mit.edu>
2010-09-04 16:41:34 -0400

    simplifying urlpatterns

--- urls.py
+++ urls.py
@@ -1,15 +1,15 @@
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('',
-                       (r'^$', 'grocheris.views.index'),
-                       (r'^stock/$', 'grocheris.views.view_in_stock'),
-                       (r'^shop/$', 'grocheris.views.view_shopping_list'),
-                       (r'^add_item/$', 'grocheris.views.add_item'),
-#                       (r'^item/(?P<item_id>\d+)/$', 'grocheris.views.view_item'),
-                       (r'^item/(?P<item_id>\d+)/buy/$', 'grocheris.views.buy_item'),
-                       (r'^item/(?P<item_id>\d+)/low/$', 'grocheris.views.low_item'),
-                       (r'^item/(?P<item_id>\d+)/out/$', 'grocheris.views.out_item'),
-#                       (r'^item/(?P<item_id>\d+)/tag/$', 'grocheris.views.tag_item'),
-#                       (r'^item/(?P<item_id>\d+)/vote/$', 'grocheris.views.vote'),
+urlpatterns = patterns('grocheris.views',
+                       (r'^$', 'index'),
+                       (r'^stock/$', 'view_in_stock'),
+                       (r'^shop/$', 'view_shopping_list'),
+                       (r'^add_item/$', 'views.add_item'),
+#                       (r'^item/(?P<item_id>\d+)/$', 'view_item'),
+                       (r'^item/(?P<item_id>\d+)/buy/$', 'buy_item'),
+                       (r'^item/(?P<item_id>\d+)/low/$', 'low_item'),
+                       (r'^item/(?P<item_id>\d+)/out/$', 'out_item'),
+#                       (r'^item/(?P<item_id>\d+)/tag/$', 'tag_item'),
+#                       (r'^item/(?P<item_id>\d+)/vote/$', 'vote'),
                        )
 
